9fd96068250585d4f952b7867cad2f0ee43b53f8
cr4sh <cr4sh-me@gmail.com>
2023-04-21 06:50:59 +0200

    Forced portrait orientation

--- app/src/main/java/com/cr4sh/nhlanucher/dialogs/FirstSetupDialog.java
+++ app/src/main/java/com/cr4sh/nhlanucher/dialogs/FirstSetupDialog.java
@@ -23,40 +23,6 @@ import com.cr4sh.nhlanucher.R;
 
 import java.util.Objects;
 
-//public class FirstSetupDialog extends DialogFragment {
-//
-//    @NonNull
-//    public Dialog onCreateDialog(Bundle savedInstanceStateExample) {
-//
-//        // Set title and message
-//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//
-//        builder.setTitle(getResources().getString(R.string.setup));
-//        builder.setMessage(getResources().getString(R.string.first_setup));
-//        setCancelable(false);
-//        builder.setIcon(android.R.drawable.ic_dialog_alert);
-//        // Setup buttons
-//        builder.setPositiveButton(getResources().getString(R.string.setup), (dialog, which) -> {
-//            // Run setup
-//            dialog.cancel();
-//            MainUtils.run_cmd("cd /root/ && apt update && apt -y install git && [ -d NHLauncher_scripts ] && rm -rf NHLauncher_scripts ; git clone https://github.com/cr4sh-me/NHLauncher_scripts || git clone https://github.com/cr4sh-me/NHLauncher_scripts && cd NHLauncher_scripts && chmod +x * && bash nhlauncher_setup.sh && exit");
-//            firstSetupCompleted();
-//        });
-//        builder.setNegativeButton(getResources().getString(R.string.cancel), (dialog, which) ->{
-//            dialog.cancel();
-//            firstSetupCompleted();
-//        });
-//        return builder.create();
-//    }
-//
-//    private void firstSetupCompleted() {
-//        SharedPreferences prefs = requireActivity().getSharedPreferences("setupSettings", Context.MODE_PRIVATE);
-//        SharedPreferences.Editor editor = prefs.edit();
-//        editor.putBoolean("isSetupCompleted", true);
-//        editor.apply();
-//    }
-//}
-
 public class FirstSetupDialog extends AppCompatDialogFragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -84,6 +50,7 @@ public class FirstSetupDialog extends AppCompatDialogFragment {
 
         cancelButton.setOnClickListener(view12 -> {
             Objects.requireNonNull(getDialog()).cancel();
+            firstSetupCompleted();
         });
 
         return view;
